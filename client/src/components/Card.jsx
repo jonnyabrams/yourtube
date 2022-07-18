7e2da381d995aaea861ac76eb3773e40950102f3
Jonny Abrams <jonnyabrams@hotmail.com>
2022-07-18 14:28:23 +0100

    Style out Card

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,13 +1,63 @@
 import styled from 'styled-components'
 
 const Container = styled.div`
+  width: 360px;
+  margin-bottom: 45px;
+  cursor: pointer;
+`
+
+const Image = styled.img`
+  width: 100%;
+  height: 202px;
+  background-color: #999;
+`
+
+const Details = styled.div`
+  display: flex;
+  margin-top: 16px;
+  gap: 12px;
+`
+
+const ChannelImage = styled.img`
+  width: 36px;
+  height: 36px;
+  border-radius: 50%;
+  background-color: #999;
+`
+
+const Texts = styled.div`
   
+`
+
+const Title = styled.h1`
+  font-size: 16px;
+  font-weight: 500;
+  color: ${({ theme }) => theme.text}
+`
+
+const ChannelName = styled.h2`
+  font-size: 14px;
+  color: ${({ theme }) => theme.textSoft};
+  margin: 9px 0px;
+`
+
+const Info = styled.div`
+  font-size: 14px;
+  color: ${({ theme }) => theme.textSoft};
 `
 
 const Card = () => {
   return (
     <Container>
-      Card
+      <Image src='' />
+      <Details>
+        <ChannelImage />
+        <Texts>
+          <Title>Test Video</Title>
+          <ChannelName>YourTube</ChannelName>
+          <Info>1,000,000 views • 8 days ago</Info>
+        </Texts>
+      </Details>
     </Container>
   )
 }
