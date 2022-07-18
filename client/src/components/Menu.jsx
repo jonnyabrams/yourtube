fc18d56d43af281dfb253b0a0892a980a0a82f4d
Jonny Abrams <jonnyabrams@hotmail.com>
2022-07-18 13:59:43 +0100

    Make login button with styling

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -54,6 +54,24 @@ const Hr = styled.hr`
   border: 0.5px solid #373737;
 `
 
+const Login = styled.div`
+
+`
+
+const Button = styled.button`
+  padding: 5px 15px;
+  background-color: transparent;
+  border: 1px solid #3ea6ff;
+  color: #3ea6ff;
+  border-radius: 3px;
+  font-weight: 500;
+  margin-top: 10px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  gap: 5px;
+`
+
 const Menu = () => {
   return (
     <Container>
@@ -83,6 +101,11 @@ const Menu = () => {
           <HistoryOutlinedIcon />
           History
         </Item>
+        <Hr />
+        <Login>
+          Sign in to like videos, comments and subscribe.
+          <Button><AccountCircleOutlinedIcon />SIGN IN</Button>
+        </Login>
         <Hr />
         <Item>
           <LibraryMusicOutlinedIcon />
