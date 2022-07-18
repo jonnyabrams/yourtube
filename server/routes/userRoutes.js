b7d832e77e798d268038eacfe1cb84776b2dea83
Jonny Abrams <jonnyabrams@hotmail.com>
2022-07-18 15:48:24 +0100

    Set up verifyToken helper for JWT

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,28 +1,29 @@
 import express from 'express'
 import { deleteUser, dislike, getUser, like, subscribe, unsubscribe, updateUser } from '../controllers/user.js'
+import { verifyToken } from '../verifyToken.js'
 
 const router = express.Router()
 
 // update user
-router.put('/:id', updateUser)
+router.put('/:id', verifyToken, updateUser)
 
 // delete user
-router.delete('/:id', deleteUser)
+router.delete('/:id', verifyToken, deleteUser)
 
 // get a user
-router.get('/find/:id', getUser)
+router.get('/find/:id', getUser) // don't need to be verified to do this
 
 // subscribe to a user
-router.put('/sub/:id', subscribe)
+router.put('/sub/:id', verifyToken, subscribe)
 
 // unsubscribe from a user
-router.put('/unsub/:id', unsubscribe)
+router.put('/unsub/:id', verifyToken, unsubscribe)
 
 // like a video
-router.put('/like/:videoId', like)
+router.put('/like/:videoId', verifyToken, like)
 
 // dislike a video
-router.put('/dislike/:videoId', dislike)
+router.put('/dislike/:videoId', verifyToken, dislike)
 
 
 export default router
