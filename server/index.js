eea8d286636839a7a3d7abb31b7f7a1c8ec56160
Jonny Abrams <jonnyabrams@hotmail.com>
2022-07-18 15:25:12 +0100

    Connect to MONGO_DB database

--- server/index.js
+++ server/index.js
@@ -5,8 +5,15 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT || 8800
+
+const connect = () => {
+  mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log('Connected to database')
+  }).catch(error => { throw error })
+}
 
 app.listen(port, () => {
+  connect()
   console.log(`⚡️[server]: Server is running at https://localhost:${port}`)
 })
