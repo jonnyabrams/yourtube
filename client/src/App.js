feb6c387d2d6c7c636619f0c4195b413f924859a
Jonny Abrams <jonnyabrams@hotmail.com>
2022-07-18 13:32:10 +0100

    Structure App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
+import styled from 'styled-components'
+import Menu from './components/Menu';
+import Navbar from './components/Navbar';
 
+const Container = styled.div``
+
+const Main = styled.div``
+
+const Wrapper = styled.div``
 
 const App = () => {
   return (
-    <div className="App">
-     
-    </div>
+    <Container>
+      <Menu />
+      <Main>
+        <Navbar />
+        <Wrapper>
+          video cards
+        </Wrapper>
+      </Main>
+    </Container>
   )
 }
 
