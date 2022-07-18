178ab4e567d3665cc92e34d7e463e7d89953f17d
Jonny Abrams <jonnyabrams@hotmail.com>
2022-07-18 14:30:09 +0100

    Style Home container & add more Cards

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,12 +2,27 @@ import styled from "styled-components"
 import Card from "../components/Card"
 
 const Container = styled.div`
-  
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
 `
 
 const Home = () => {
   return (
     <Container>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
       <Card />
       <Card />
       <Card />
