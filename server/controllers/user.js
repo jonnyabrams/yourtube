00662f035818b849424cde0489449f6c3b5c05d1
Jonny Abrams <jonnyabrams@hotmail.com>
2022-07-18 15:46:06 +0100

    Link user routes with controllers

---
+++ server/controllers/user.js
@@ -0,0 +1,27 @@
+export const updateUser = async (req, res, next) => {
+
+}
+
+export const deleteUser = async (req, res, next) => {
+
+}
+
+export const getUser = async (req, res, next) => {
+
+}
+
+export const subscribe = async (req, res, next) => {
+
+}
+
+export const unsubscribe = async (req, res, next) => {
+
+}
+
+export const like = async (req, res, next) => {
+
+}
+
+export const dislike = async (req, res, next) => {
+
+}
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,28 @@
 import express from 'express'
+import { deleteUser, dislike, getUser, like, subscribe, unsubscribe, updateUser } from '../controllers/user.js'
 
 const router = express.Router()
+
+// update user
+router.put('/:id', updateUser)
+
+// delete user
+router.delete('/:id', deleteUser)
+
+// get a user
+router.get('/find/:id', getUser)
+
+// subscribe to a user
+router.put('/sub/:id', subscribe)
+
+// unsubscribe from a user
+router.put('/unsub/:id', unsubscribe)
+
+// like a video
+router.put('/like/:videoId', like)
+
+// dislike a video
+router.put('/dislike/:videoId', dislike)
+
 
 export default router
