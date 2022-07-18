3bea15a04944568d92391b3e608e77523e7be430
Jonny Abrams <jonnyabrams@hotmail.com>
2022-07-18 15:36:58 +0100

    Add error middleware

--- server/index.js
+++ server/index.js
@@ -24,6 +24,16 @@ const connect = () => {
   }).catch(error => { throw error })
 }
 
+app.use((err, req, res, next) => {
+  const status = err.status || 500
+  const message = err.message || "Something went wrong!"
+  return res.status(status).json({
+    success: false,
+    status,
+    message
+  })
+})
+
 app.listen(port, () => {
   connect()
   console.log(`⚡️[server]: Server is running at https://localhost:${port}`)
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,5 +1,15 @@
 import express from 'express'
+import { signup } from '../controllers/authController'
 
 const router = express.Router()
+
+// create user
+router.post('/signup', signup)
+
+// log in
+router.post('/login', )
+
+// google authentication
+router.post('/google', )
 
 export default router
