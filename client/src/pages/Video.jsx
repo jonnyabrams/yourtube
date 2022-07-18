38a14ebaa64a3b65c8c709b5ca16458d5869329c
Jonny Abrams <jonnyabrams@hotmail.com>
2022-07-18 14:53:50 +0100

    More Video styles/arrangement

--- client/src/pages/Video.jsx
+++ client/src/pages/Video.jsx
@@ -57,6 +57,54 @@ const Recommendation = styled.div`
   flex: 2;
 `
 
+const Channel = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+
+const ChannelInfo = styled.div`
+  display: flex;
+  gap: 20px;
+`
+
+const Image = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+`
+
+const ChannelDetail = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: ${({ theme }) => theme.text}
+`
+
+const ChannelName = styled.span`
+  font-weight: 500;
+`
+
+const ChannelCounter = styled.span`
+  margin-top: 5px;
+  margin-bottom: 20px;
+  color: ${({ theme }) => theme.textSoft};
+  font-size: 12px;
+`
+
+const ChannelDescription = styled.p`
+  font-size: 14px;
+`
+
+const Subscribe = styled.button`
+  background-color: #cc1a00;
+  font-weight: 500;
+  color: white;
+  border: none;
+  border-radius: 3px;
+  height: max-content;
+  padding: 10px 20px;
+  cursor: pointer;
+`
+
 const Video = () => {
   return (
     <Container>
@@ -83,6 +131,17 @@ const Video = () => {
           </Buttons>
         </Details>
         <Hr />
+        <Channel>
+          <ChannelInfo>
+            <Image src='https://yt3.ggpht.com/ytc/AKedOLQOuaQIk0NJjjw5bbsv9v9CrDvNKRVjGii426be4Q=s176-c-k-c0x00ffffff-no-rj-mo' />
+            <ChannelDetail>
+              <ChannelName>The Iain Duncan Smiths</ChannelName>
+              <ChannelCounter>13k subscribers</ChannelCounter>
+              <ChannelDescription>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae sint eos atque ex voluptatibus eum nihil quo nisi aperiam at suscipit, nobis ducimus nostrum debitis sapiente assumenda vero praesentium. Consectetur?</ChannelDescription>
+            </ChannelDetail>
+          </ChannelInfo>
+          <Subscribe>SUBSCRIBE</Subscribe>
+        </Channel>
       </Content>
       <Recommendation>
         recommendation
