964b19ab42150dc578f40356db05bd9d1d81b19b
Jonny Abrams <jonnyabrams@hotmail.com>
2022-07-19 11:02:14 +0100

    Menu has no login button when user is signed in

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -17,6 +17,7 @@ import FlagOutlinedIcon from "@mui/icons-material/FlagOutlined"
 import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined"
 import SettingsBrightnessOutlinedIcon from "@mui/icons-material/SettingsBrightnessOutlined"
 import { Link } from "react-router-dom"
+import { useSelector } from "react-redux"
 
 const Container = styled.div`
   flex: 1;
@@ -87,6 +88,8 @@ const Title = styled.h2`
 `
 
 const Menu = ({ darkMode, setDarkMode }) => {
+  const currentUser = useSelector(state => state.user.currentUser)
+
   return (
     <Container>
       <Wrapper>
@@ -124,13 +127,13 @@ const Menu = ({ darkMode, setDarkMode }) => {
           History
         </Item>
         <Hr />
-        <Login>
+        {!currentUser && <><Login>
           Sign in to like videos, comments and subscribe.
           <Link to='/login' style={{ textDecoration: 'none', color: 'inherit' }}>
             <Button><AccountCircleOutlinedIcon />SIGN IN</Button>
           </Link>
         </Login>
-        <Hr />
+        <Hr /></>}
         <Title>BEST OF YOURTUBE</Title>
         <Item>
           <LibraryMusicOutlinedIcon />
