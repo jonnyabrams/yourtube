ba0b7fda2ace285a2b7cd278519b6c301b7e61b4
Jonny Abrams <jonnyabrams@hotmail.com>
2022-07-19 10:46:25 +0100

    Set up Redux store

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react"
 import styled from "styled-components"
 import axios from 'axios'
+import { useDispatch } from "react-redux"
+import { loginFailure, loginStart, loginSuccess } from "../redux/userSlice"
 
 const Container = styled.div`
   display: flex;
@@ -67,14 +69,16 @@ const Login = () => {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const dispatch = useDispatch()
 
   const handleLogin = async (e) => {
     e.preventDefault()
+    dispatch(loginStart())
     try {
       const res = await axios.post('/auth/login', { name, password })
-      console.log(res)
+      dispatch(loginSuccess(res.data))
     } catch (error) {
-      
+      dispatch(loginFailure())
     }
   }
   
