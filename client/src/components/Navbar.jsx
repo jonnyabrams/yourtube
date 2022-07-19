dfafd58cb3ef306282738e2ad7a40104e2f3d3b3
Jonny Abrams <jonnyabrams@hotmail.com>
2022-07-19 16:17:12 +0100

    Create Upload popup

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,6 +4,8 @@ import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined"
 import VideoCallOutlinedIcon from "@mui/icons-material/VideoCallOutlined"
 import { Link } from "react-router-dom"
 import { useSelector } from 'react-redux'
+import { useState } from "react"
+import Upload from "./Upload"
 
 const Container = styled.div`
   position: sticky;
@@ -72,30 +74,34 @@ const Avatar = styled.img`
 
 const Navbar = () => {
   const currentUser = useSelector(state => state.user.currentUser)
+  const [open, setOpen] = useState(false)
 
   return (
-    <Container>
-      <Wrapper>
-        <Search>
-          <Input placeholder='Search' />
-          <SearchOutlinedIcon />
-        </Search>
-        {currentUser ? (
-          <User>
-            <VideoCallOutlinedIcon />
-            <Avatar src={currentUser.img} />
-            {currentUser.name}
-          </User>
-        ) : (
-          <Link to='/login' style={{ textDecoration: 'none', color: 'inherit' }}>
-            <Button>
-              <AccountCircleOutlinedIcon />
-              SIGN IN
-            </Button>
-          </Link>
-        )}
-      </Wrapper>
-    </Container>
+    <>
+      <Container>
+        <Wrapper>
+          <Search>
+            <Input placeholder='Search' />
+            <SearchOutlinedIcon />
+          </Search>
+          {currentUser ? (
+            <User>
+              <VideoCallOutlinedIcon onClick={() => setOpen(true)} />
+              <Avatar src={currentUser.img} />
+              {currentUser.name}
+            </User>
+          ) : (
+            <Link to='/login' style={{ textDecoration: 'none', color: 'inherit' }}>
+              <Button>
+                <AccountCircleOutlinedIcon />
+                SIGN IN
+              </Button>
+            </Link>
+          )}
+        </Wrapper>
+      </Container>
+      {open && <Upload setOpen={setOpen} />}
+    </>
   )
 }
 
---
+++ client/src/components/Upload.jsx
@@ -0,0 +1,87 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: #000000a7;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const Wrapper = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: ${({ theme }) => theme.bgLighter};
+  color: ${({ theme }) => theme.text};
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  position: relative;
+`
+
+const Close = styled.div`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  cursor: pointer;
+`
+
+const Title = styled.h1`
+  text-align: center;
+`
+
+const Input = styled.input`
+  border: 1px solid ${({ theme }) => theme.soft};
+  color: ${({ theme }) => theme.text};
+  border-radius: 3px;
+  padding: 10px;
+  background-color: transparent;
+`
+
+const Description = styled.textarea`
+  border: 1px solid ${({ theme }) => theme.soft};
+  color: ${({ theme }) => theme.text};
+  border-radius: 3px;
+  padding: 10px;
+  background-color: transparent;
+`
+
+const Button = styled.button`
+  border-radius: 3px;
+  border: none;
+  padding: 10px 20px;
+  font-weight: 500;
+  cursor: pointer;
+  background-color: ${({ theme }) => theme.soft};
+  color: ${({ theme }) => theme.textSoft};
+`;
+
+const Label = styled.label`
+  font-size: 14px;
+`;
+
+const Upload = ({ setOpen }) => {
+  return (
+    <Container>
+      <Wrapper>
+        <Close onClick={() => setOpen(false)}>X</Close>
+        <Title>Upload a new video</Title>
+        <Label>Video:</Label>
+        <Input type='file' accept='video/*' />
+        <Input type='text' placeholder='Title' />
+        <Description placeholder='Description' rows={8} />
+        <Input type='text' placeholder='Separate the tags with commas' />
+        <Label>Image:</Label>
+        <Input type='file' accept='image/*' />
+        <Button>Upload</Button>
+      </Wrapper>
+    </Container>
+  )
+}
+
+export default Upload
